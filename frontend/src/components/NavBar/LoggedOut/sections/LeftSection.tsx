9f4e4ef5a8cb8a3eb01d18ecd15d28d8ea2a1d6e
mogaya <mogayainnocent@gmail.com>
2025-04-03 22:41:40 +0300

    nav bar logged out right and leftsection

--- frontend/src/components/NavBar/LoggedOut/sections/LeftSection.tsx
+++ frontend/src/components/NavBar/LoggedOut/sections/LeftSection.tsx
@@ -1,12 +1,12 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import HamburgerMenu from "../../components/HamburgerMenu";
-import logo from "../../../../assets/logo.png";
+import Logo from "../../components/Logo";
 
 const LeftSection = () => {
   return (
     <Flex alignItems={"center"} justifyContent={"space-between"}>
       <HamburgerMenu />
-      <Image src={logo} w={"50px"} h={"auto"} />
+      <Logo />
     </Flex>
   );
 };
