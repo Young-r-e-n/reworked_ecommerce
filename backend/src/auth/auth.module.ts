352d2239f6035baa7c6bc5ec23f8476a434a0760
Young-r-e-n <youngrenowola@gmail.com>
2025-04-04 08:22:02 +0300

    fixed auth

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';  
 import { PrismaModule } from '../prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule, JwtModule.register({ secret: 'secretKey', signOptions: { expiresIn: '1h' } })],
+  imports: [
+    PrismaModule,
+    JwtModule.register({ secret: 'secretKey', signOptions: { expiresIn: '1h' } }),
+  ],
+  controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
 })
