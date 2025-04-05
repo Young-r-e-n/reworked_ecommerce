ea06120223ee87308191b32453cd576f3b81e1ba
mogaya <mogayainnocent@gmail.com>
2025-04-05 16:40:19 +0300

    admin routes setup

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -5,6 +5,13 @@ import Dashboard from "../pages/VendorPages/Dashboard";
 import Wallet from "../pages/VendorPages/Wallet";
 import Products from "../pages/VendorPages/MyShop";
 import Orders from "../pages/VendorPages/Orders";
+import AdminDashboard from "../pages/AdminPages/AdminDashboard";
+import PaymentView from "../pages/AdminPages/PaymentView";
+import Marketing from "../pages/AdminPages/Marketing";
+import AllProducts from "../pages/AdminPages/AllProducts";
+import AllOrders from "../pages/AdminPages/AllOrders";
+import BuyerView from "../pages/AdminPages/BuyerView";
+import VendorView from "../pages/AdminPages/VendorView";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +27,15 @@ const router = createBrowserRouter([
       { path: "/vendor/wallet", element: <Wallet /> },
       { path: "/vendor/add", element: <Wallet /> },
       { path: "/vendor/wallet", element: <Wallet /> },
+
+      // Admin Routes
+      { path: "/admin/dashboard", element: <AdminDashboard /> },
+      { path: "/admin/payments", element: <PaymentView /> },
+      { path: "/admin/marketing", element: <Marketing /> },
+      { path: "/admin/allproducts", element: <AllProducts /> },
+      { path: "/admin/allorders", element: <AllOrders /> },
+      { path: "/admin/buyerview", element: <BuyerView /> },
+      { path: "/admin/vendorview", element: <VendorView /> },
     ],
   },
 ]);
